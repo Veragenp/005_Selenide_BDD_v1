90d41be3915de02d5cc528e27d2994e5e44d0822
Yury <yp@unotex.ru>
2022-06-09 10:43:28 +0700

    for experiment

--- src/test/java/ru/netology/web/page/TransactionPage.java
+++ src/test/java/ru/netology/web/page/TransactionPage.java
@@ -4,7 +4,6 @@ import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.Keys;
 import ru.netology.web.data.DataHelper;
 
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.$x;
 
